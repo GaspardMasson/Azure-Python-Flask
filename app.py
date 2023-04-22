c098b06b1b1c8ad0626ce7a469d0a2de315255a0
Gaspard Masson <78444117+GaspardMasson@users.noreply.github.com>
2023-04-23 00:11:30 +0200

    try to add webhook

--- app.py
+++ app.py
@@ -4,3 +4,31 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    event_type = request.headers.get('X-Strava-Event-Type')
+    if event_type == 'activity.create':
+        activity_data = request.json
+        # Traitez les données de la nouvelle activité ici
+        return jsonify({'success': True}), 200
+    else:
+        return jsonify({'success': False}), 400
+
+if __name__ == '__main__':
+    app.run()
+
+
+#https://stravaappwebhook.azurewebsites.net/webhook
