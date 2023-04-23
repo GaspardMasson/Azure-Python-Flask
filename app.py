9d07d4563849b7aab122106ac945321370a51407
Gaspard Masson <78444117+GaspardMasson@users.noreply.github.com>
2023-04-23 13:05:25 +0200

    add webhook

--- app.py
+++ app.py
@@ -5,7 +5,19 @@ app = Flask(__name__)
 def hello():
     return "Hello, Worldddddddd!"
 
+from flask import Flask, request, abort
 
+app = Flask(__name__)
 
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        print(request.json)
+        return 'success', 200
+    else:
+        abort(400)
+
+if __name__ == '__main__':
+    app.run()
 
 #https://stravaappwebhook.azurewebsites.net/webhook
